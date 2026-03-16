e42f5d4e2e16152bccc29add92fcc481ff1b6332
tvhgn <tom.van.hogen@gmail.com>
2026-03-16 16:13:36 +0100

    deleted unnecessary files

--- modules/helpers/effects.py
+++
@@ -1,5 +0,0 @@
-def strike(text):
-    result = ''
-    for c in text:
-        result = result + c + '\u0336'
-    return result
--- modules/helpers/prompts.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def continue_and_clear():
-    # Print message and wait for input
-    input("\n\nPress enter to continue... ")
-    
-    # Clear the screen    
-    os.system("clear")
